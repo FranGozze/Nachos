eef2cc50d7a573bc6939800164257664e3dca95c
franco <fgozzerino@gmail.com>
2024-06-23 18:04:43 -0300

    Vuelvo a agregar coremap

--- nachos-unr24a/code/lib/bitmap.cc
+++ nachos-unr24a/code/lib/bitmap.cc
@@ -6,11 +6,9 @@
 /// All rights reserved.  See `copyright.h` for copyright notice and
 /// limitation of liability and disclaimer of warranty provisions.
 
-
 #include "bitmap.hh"
 
 #include <stdio.h>
-
 
 /// Initialize a bitmap with `nitems` bits, so that every bit is clear.  It
 /// can be added somewhere on a list.
@@ -18,66 +16,71 @@
 /// * `nitems` is the number of bits in the bitmap.
 Bitmap::Bitmap(unsigned nitems)
 {
-    ASSERT(nitems > 0);
+  ASSERT(nitems > 0);
 
-    numBits  = nitems;
-    numWords = DivRoundUp(numBits, BITS_IN_WORD);
-    map      = new unsigned [numWords];
-    for (unsigned i = 0; i < numBits; i++) {
-        Clear(i);
-    }
+  numBits = nitems;
+  numWords = DivRoundUp(numBits, BITS_IN_WORD);
+  map = new unsigned[numWords];
+#ifdef SWAP
+  coremapEntries = new Coremap[nitems];
+#endif
+  for (unsigned i = 0; i < numBits; i++)
+  {
+    Clear(i);
+  }
 }
 
 /// De-allocate a bitmap.
 Bitmap::~Bitmap()
 {
-    delete [] map;
+  delete[] map;
+#ifdef SWAP
+  delete[] coremapEntries;
+#endif
 }
 
 /// Set the “nth” bit in a bitmap.
 ///
 /// * `which` is the number of the bit to be set.
-void
-Bitmap::Mark(unsigned which)
+void Bitmap::Mark(unsigned which)
 {
-    ASSERT(which < numBits);
-    map[which / BITS_IN_WORD] |= 1 << which % BITS_IN_WORD;
+  ASSERT(which < numBits);
+  map[which / BITS_IN_WORD] |= 1 << which % BITS_IN_WORD;
 }
 
 /// Clear the “nth” bit in a bitmap.
 ///
 /// * `which` is the number of the bit to be cleared.
-void
-Bitmap::Clear(unsigned which)
+void Bitmap::Clear(unsigned which)
 {
-    ASSERT(which < numBits);
-    map[which / BITS_IN_WORD] &= ~(1 << which % BITS_IN_WORD);
+  ASSERT(which < numBits);
+  map[which / BITS_IN_WORD] &= ~(1 << which % BITS_IN_WORD);
 }
 
 /// Return true if the “nth” bit is set.
 ///
 /// * `which` is the number of the bit to be tested.
-bool
-Bitmap::Test(unsigned which) const
+bool Bitmap::Test(unsigned which) const
 {
-    ASSERT(which < numBits);
-    return map[which / BITS_IN_WORD] & 1 << which % BITS_IN_WORD;
+  ASSERT(which < numBits);
+  return map[which / BITS_IN_WORD] & 1 << which % BITS_IN_WORD;
 }
 
 /// Return the number of the first bit which is clear.  As a side effect, set
 /// the bit (mark it as in use).  (In other words, find and allocate a bit.)
 ///
 /// If no bits are clear, return -1.
-int
-Bitmap::Find()
+int Bitmap::Find()
 {
-    for (unsigned i = 0; i < numBits; i++) {
-        if (!Test(i)) {
-            Mark(i);
-            return i;
-        }
+  for (unsigned i = 0; i < numBits; i++)
+  {
+    if (!Test(i))
+    {
+      Mark(i);
+      return i;
     }
-    return -1;
+  }
+  return -1;
 }
 
 /// Return the number of clear bits in the bitmap.  (In other words, how many
@@ -85,30 +88,33 @@ Bitmap::Find()
 unsigned
 Bitmap::CountClear() const
 {
-    unsigned count = 0;
+  unsigned count = 0;
 
-    for (unsigned i = 0; i < numBits; i++) {
-        if (!Test(i)) {
-            count++;
-        }
+  for (unsigned i = 0; i < numBits; i++)
+  {
+    if (!Test(i))
+    {
+      count++;
     }
-    return count;
+  }
+  return count;
 }
 
 /// Print the contents of the bitmap, for debugging.
 ///
 /// Could be done in a number of ways, but we just print the indexes of all
 /// the bits that are set in the bitmap.
-void
-Bitmap::Print() const
+void Bitmap::Print() const
 {
-    printf("Bitmap bits set:\n");
-    for (unsigned i = 0; i < numBits; i++) {
-        if (Test(i)) {
-            printf("%u ", i);
-        }
+  printf("Bitmap bits set:\n");
+  for (unsigned i = 0; i < numBits; i++)
+  {
+    if (Test(i))
+    {
+      printf("%u ", i);
     }
-    printf("\n");
+  }
+  printf("\n");
 }
 
 /// Initialize the contents of a bitmap from a Nachos file.
@@ -116,11 +122,10 @@ Bitmap::Print() const
 /// Note: this is not needed until the *FILESYS* assignment.
 ///
 /// * `file` is the place to read the bitmap from.
-void
-Bitmap::FetchFrom(OpenFile *file)
+void Bitmap::FetchFrom(OpenFile *file)
 {
-    ASSERT(file != nullptr);
-    file->ReadAt((char *) map, numWords * sizeof (unsigned), 0);
+  ASSERT(file != nullptr);
+  file->ReadAt((char *)map, numWords * sizeof(unsigned), 0);
 }
 
 /// Store the contents of a bitmap to a Nachos file.
@@ -128,9 +133,8 @@ Bitmap::FetchFrom(OpenFile *file)
 /// Note: this is not needed until the *FILESYS* assignment.
 ///
 /// * `file` is the place to write the bitmap to.
-void
-Bitmap::WriteBack(OpenFile *file) const
+void Bitmap::WriteBack(OpenFile *file) const
 {
-    ASSERT(file != nullptr);
-    file->WriteAt((char *) map, numWords * sizeof (unsigned), 0);
+  ASSERT(file != nullptr);
+  file->WriteAt((char *)map, numWords * sizeof(unsigned), 0);
 }
